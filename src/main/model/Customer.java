1a249077da773c0ab975f36c90d8e536dd7cee16
Kavyansh Bansal <kavyansheduv@gmail.com>
2022-10-15 17:48:11 -0700

    completed phase1 of the project

---
+++ src/main/model/Customer.java
@@ -0,0 +1,132 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Customer {
+
+    int totalAmountSpent = 0;
+
+    private ArrayList<String> expenselist = new ArrayList();
+    private ArrayList<Integer> itemprice = new ArrayList();
+    private ArrayList<String> expensedate = new ArrayList();
+    private int expenselimit = 2000; // by default user has expense limit of $2000
+    private int size;
+    private int id;
+
+    private String customername;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCustomername() {
+        return customername;
+    }
+
+    public void setCustomername(String customername) {
+        this.customername = customername;
+    }
+
+    //REQUIRES : price>0
+    //EFFECTS : This method is used to set the name of the customer and set up an id which they can use later for
+    // retrieving the data
+    public Customer(String customername, int id) {
+        setCustomername(customername);
+        setId(id);
+    }
+
+
+    //REQUIRES : price>0
+    //MODIFIES: this
+    //EFFECTS: It is bascially used to store the name of the expense of the user along with the price and dateof expense
+    //- This method only works if the expense limit which is set up by the user is currently more than total_amount
+    //  of expenses made by the user. If the user is willing to add an expense which will exceed the expense limit
+    //  then the method will ask the user to update their expense limit so they can add the expense along with the date
+    // This method also increments the size of the arraylist
+    public String addExpenses(String expensename, int price, String date) {
+
+
+        if (expenselimit < (totalAmountSpent + price)) {
+            return "You will cross your expense limit of $ " + getExpenseLimit() + " :" + " "
+                    + "If you will still like to store this expense please update your expense limit";
+        } else {
+
+            expenselist.add(expensename);
+            expensedate.add(date);
+            itemprice.add(price);
+            totalAmountSpent += price;
+            size++;
+
+            return "Expense:  " + expenselist.get(expenselist.size() - 1) + " with the price being : "
+                    + itemprice.get(itemprice.size() - 1) + " has been added successfully on "
+                    + expensedate.get(expensedate.size() - 1);
+
+        }
+    }
+
+    //    by default user is assigned an expense limit of $2000
+    public void setExpenseLimit(int expenselimit) {
+
+        this.expenselimit = expenselimit;
+    }
+
+    public int getExpenseLimit() {
+
+        return this.expenselimit;
+    }
+
+    public int getTotalAmountSpent() {
+        return this.totalAmountSpent;
+    }
+
+
+    // This method is used to display all the  expenses of the user
+    public String displayExpenses() {
+        String s = "";
+        for (int i = 0; i < expenselist.size(); i++) {
+            s = s + "Expense:  " + expenselist.get(i) + " with the price being : "
+                    + itemprice.get(i) + " was purchased on "
+                    + expensedate.get(i) + "\n";
+        }
+        return s;
+
+
+    }
+//    REQUIRES: index>0
+//    MODIFIES: this
+//    EFFECTS: This method is used to completely remove a particular expense of the user
+
+    public void removeExpense(int index) {
+
+        expenselist.remove(index);
+        itemprice.remove(index);
+        expensedate.remove(index);
+        size--;
+
+
+    }
+
+    public ArrayList getExpenselist() {
+
+        return expenselist;
+    }
+
+    public ArrayList getitemprice() {
+
+        return itemprice;
+    }
+
+    public ArrayList getExpensedate() {
+        return expensedate;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+}
--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,7 +1,106 @@
 package ui;
 
-public class Main {
-    public static void main(String[] args) {
+import model.Customer;
 
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner r = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        String customername = r.nextLine();
+
+
+        System.out.println("Enter your id which will be used by you for accessing your data later: ");
+        int id = r.nextInt();
+
+        Customer user = new Customer(customername,id);
+
+        menuformain(user);
+
+    }
+
+    @SuppressWarnings("checkstyle:MethodLength")
+    public static void updateexpenselimit(Customer c, int price) {
+        int expenselimit = 0;
+
+        Scanner r = new Scanner(System.in);
+        if (c.getExpenseLimit() > (c.getTotalAmountSpent() + price)) {
+            return;
+        }
+        System.out.print("Your expense limit is not enough to add this expense please update it   ");
+        System.out.println();
+        System.out.print("If you do not want to update your limit and end the program type No else type ");
+        System.out.println("yes:  ");
+        char response = r.nextLine().charAt(0);
+
+        if (response == 'N' || response == 'n') {
+            System.exit(0);
+        } else {
+            System.out.println(" What is the expense limit you want your limit to be updated to ??:  ");
+            expenselimit = r.nextInt();
+
+            c.setExpenseLimit(expenselimit);
+
+            r.nextLine();
+        }
+
+        while (c.getExpenseLimit() < (c.getTotalAmountSpent() + price)) {
+            System.out.println("This limit is still not enough to purchase the goods : ");
+            System.out.println();
+            System.out.println("Please update your limit. Enter yes to update it again or No to end the program : ");
+
+            char userresponse = r.nextLine().charAt(0);
+
+            if (userresponse == 'Y' || userresponse == 'y') {
+                System.out.println("Enter the new limit: ");
+                expenselimit = r.nextInt();
+                c.setExpenseLimit(expenselimit);
+                r.nextLine();
+            } else {
+                System.exit(0);
+            }
+        }
+        System.out.println("Thanks for updating your expense limit to " + c.getExpenseLimit());
+    }
+
+
+    @SuppressWarnings("checkstyle:MethodLength")
+    public static void menuformain(Customer c) {
+
+        Scanner r = new Scanner(System.in);
+
+
+        System.out.println("Welcome  " + c.getCustomername() + " you have choosen ID number:  " + c.getId());
+
+        boolean input = false;
+        while (!input) {
+            System.out.println("Please enter the name, price and date of your expense: ");
+            String name = r.nextLine();
+            int price = r.nextInt();
+            r.nextLine();
+            String date = r.nextLine();
+            updateexpenselimit(c, price);
+
+            System.out.println(c.addExpenses(name, price, date));
+
+            System.out.println("Do you want to enter any other expense Type yes or no :  ");
+            char userresponse = r.nextLine().charAt(0);
+
+            if (userresponse == 'Y' || userresponse == 'y') {
+                input = false;
+            } else {
+                input = true;
+            }
+
+
+        }
+        System.out.println("To view the history of expenses please type 1 else type something to end the program : ");
+        char history = r.nextLine().charAt(0);
+        if (history == '1') {
+            System.out.println(c.displayExpenses());
+        }
     }
 }
