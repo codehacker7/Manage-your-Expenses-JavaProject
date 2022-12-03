faf375e098f8892e84dede542a9faf9abb1a0853
Kavyansh Bansal <kavyansheduv@gmail.com>
2022-12-02 18:48:59 -0800

    phase 4 commit-updated 4

--- src/main/model/EventLogging.java
+++ src/main/model/EventLogging.java
@@ -2,10 +2,15 @@ package model;
 
 import java.util.List;
 
+/*
+This class is used to handle all the expenses which are to be printed on the console
+ */
+
 public class EventLogging {
 
     private static int count = 0;
 
+    //EFFECTS: This method is used to print all the expenses added to the system
     public static void addExpensesLogger(Customer c) {
 
         String s = "";
@@ -16,6 +21,7 @@ public class EventLogging {
         eventLogger("New " + s + " has been successfully added");
     }
 
+    //EFFECTS: This method is used to print the deatils of new customer
     public static void saveaddCustomer(int id, String customername) {
         if (id != -1 && !(customername.equals("No details found"))) {
             eventLogger("Thank you " + customername + " for saving details with id number " + id);
@@ -23,6 +29,7 @@ public class EventLogging {
 
     }
 
+    //EFFECTS: This method is used to print the deatils of old customer
     public static void loadCustomer(int id, String customername) {
         if (id != -1 && !(customername.equals("No details found"))) {
             eventLogger("Thank you " + customername + " for viewing your expenses " + id);
@@ -30,6 +37,7 @@ public class EventLogging {
 
     }
 
+    //EFFECTS: This method is used to print the all the expenses saved in the system
     public static void loadexpenses(List<Expenses> ex, Customer customer) {
         String s = "";
         for (Expenses exp : customer.getreadCustomerExpense()) {
@@ -44,17 +52,19 @@ public class EventLogging {
 
     }
 
+    //EFFECTS: This method is used to print that all the expenses has been updated
     public static void expensesLimitupdated(double updatedlimit) {
         eventLogger("Your expense limit has been updated to " + updatedlimit);
     }
 
-
+    //EFFECTS: This method is used to create an event log
     public static void eventLogger(String details) {
         EventLog eventLog = EventLog.getInstance();
         eventLog.logEvent(new Event(details));
 
     }
 
+    //EFFECTS: This method is used to print all the events in the eventlog at the end of the system
     public static void printEventsInLog() {
         for (Event e : EventLog.getInstance()) {
             System.out.println(e.toString() + "\n");
